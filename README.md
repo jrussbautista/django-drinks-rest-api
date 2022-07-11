72875f79a2dd3629a0067014e2103bce4c987b29
James Russel C. Bautista <jrussclay@gmail.com>
2022-07-11 23:19:47 +0800

    Add crud functionality

---
+++ README.md
@@ -0,0 +1,5 @@
+## DRINKS REST API
+
+### Example of basics CRUD functionality using Django and DRF.
+
+### Course link: https://www.youtube.com/watch?v=i5JykvxUk_A
---
+++ drinks/views.py
@@ -0,0 +1,41 @@
+from .models import Drink
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET', 'POST'])
+def drink_list(request):
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+            serializer = DrinkSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def drink_detail(request, id):
+    try:        
+        drink = Drink.objects.get(id=id)
+    except Drink.DoesNotExist:
+        return Response({ "message": "Drink not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink, many=False)
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        drink.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    if request.method == 'PUT':
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
